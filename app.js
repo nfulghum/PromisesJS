be1366ff3a0670cfe2968d059b6a451dd959dee3
Nick Fulghum <nickfulghum92@gmail.com>
2022-09-21 13:21:42 -0500

    question 1 completed

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-let url = `http://numbersapi.com/${num}?json`
+// Part 1
+let url = 'http://numbersapi.com';
+let favNum = 23;
+
+let favNumFact = axios.get(`${url}/${favNum}?json`)
+console.log(favNumFact)
+
 

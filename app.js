e2b4af364d84bceb11a66accff824dd632270c88
Nick Fulghum <nickfulghum92@gmail.com>
2022-09-21 13:27:48 -0500

    second question

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ let favNumFact = axios.get(`${url}/${favNum}?json`)
 console.log(favNumFact)
 
 
+// Part 2
+let favNums = [23, 92, 30];
+
+axios.get(`${url}/${favNums}`)
+    .then(res => {
+        console.log("First fact")
+        console.log(res.data)
+        return axios.get(res.data.text)
+    })
+    .then(res => {
+        console.log("Second fact")
+        console.log(res.data)
+        return axios.get(res.data.text)
+    })
+    .then(res => {
+        console.log("Third fact")
+        console.log(res.data)
+    })
+    .catch(err => console.log("rejected", err))
